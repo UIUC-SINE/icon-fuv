d8a39f80e8bb96e02b7ea320d8f4e77ad3fd2497
ukamaci <kamaci.ulas@gmail.com>
2020-05-23 14:35:32 -0500

    update the uncertainty calculator to accept background parameters

--- python3/scripts/uncertainty_calculator.py
+++ python3/scripts/uncertainty_calculator.py
@@ -1,5 +1,5 @@
-# Ulas Kamaci - 2020-05-22
-# uncertainty_calculation v1.0
+# Ulas Kamaci - 2020-05-23
+# uncertainty_calculation v1.1
 # uncertainty_calculator is the main function where gain_calculator_day and
 # night are called within that.
 
@@ -32,28 +32,28 @@ def gain_calculator_day(signalday,background_std,profile_flatfield):
 
     # find the indices corresponding to the top 10 percent of the low pass signal
     # (excluding the top 1 percent since there might be outliers)
-    idx1 = np.argsort(signalday_lp)[
+    index1 = np.argsort(signalday_lp)[
         int(0.9 * len(signalday_lp)) : int(0.99 * len(signalday_lp))
     ]
 
     # make sure these indices are larger than some small positive number, say 2
-    idx_p = np.where(signalday_lp[idx1] > 2)[0]
+    index_p = np.where(signalday_lp[index1] > 2)[0]
 
     # update the indices with the positivity restriction
-    idx = idx1[idx_p]
+    index = index1[index_p]
 
     # detrend the signal and normalize (not a complete normalization) such that
     # its sample variance is equal to the gain
     detrended_normalized = (
-        (signalday[idx] - signalday_lp[idx]) /
-        np.sqrt(abs(signalday_lp[idx]) / profile_flatfield[idx])
+        (signalday[index] - signalday_lp[index]) /
+        np.sqrt(abs(signalday_lp[index]) / profile_flatfield[index])
     )
 
     # compute the background noise contribution to the sample variance of the
     # detrended_normalized array
     background_contributor = np.mean(
-        (background_std**2)[idx] /
-        (abs(signalday_lp[idx]) / profile_flatfield[idx])
+        (background_std**2)[index] /
+        (abs(signalday_lp[index]) / profile_flatfield[index])
     )
 
     gain_day = np.std(detrended_normalized)**2 - background_contributor
@@ -83,35 +83,35 @@ def gain_calculator_night(signalnight,profile_flatfield):
 
     # find the indices corresponding to the smallest 10 percent of the low pass signal
     # to characterize the background variance (more reliable than the saa data)
-    idx_bkg = np.argsort(signalnight_lp)[ : int(0.1 * len(signalnight_lp))]
+    index_bkg = np.argsort(signalnight_lp)[ : int(0.1 * len(signalnight_lp))]
 
     # compute the background fluctuations by detrending the corresponding data
-    background_data = signalnight[idx_bkg] - signalnight_lp[idx_bkg]
+    background_data = signalnight[index_bkg] - signalnight_lp[index_bkg]
     background_data_var = np.std(background_data)**2
 
     # find the indices corresponding to the lowest 95 percent of the low pass signal
     # (excluding the top 5 percent since there might be stars)
-    idx1 = np.argsort(signalnight_lp)[ : int(0.95 * len(signalnight_lp))]
+    index1 = np.argsort(signalnight_lp)[ : int(0.95 * len(signalnight_lp))]
 
     # make sure these indices are larger than some small positive number, say 2
     # so that we get rid of the negative values
-    idx_p = np.where(signalnight_lp[idx1] > 2)[0]
+    index_p = np.where(signalnight_lp[index1] > 2)[0]
 
     # update the indices with the positivity restriction
-    idx = idx1[idx_p]
+    index = index1[index_p]
 
     # detrend the signal and normalize (not a complete normalization) such that
     # its sample variance is equal to the gain
     detrended_normalized = (
-        (signalnight[idx] - signalnight_lp[idx]) /
-        np.sqrt(abs(signalnight_lp[idx]) / profile_flatfield[idx])
+        (signalnight[index] - signalnight_lp[index]) /
+        np.sqrt(abs(signalnight_lp[index]) / profile_flatfield[index])
     )
 
     # compute the background noise contribution to the sample variance of the
     # detrended_normalized array
     background_contributor = np.mean(
         background_data_var /
-        (abs(signalnight_lp[idx]) / profile_flatfield[idx])
+        (abs(signalnight_lp[index]) / profile_flatfield[index])
     )
 
     gain_night = np.std(detrended_normalized)**2 - background_contributor
@@ -120,10 +120,12 @@ def gain_calculator_night(signalnight,profile_flatfield):
 
 def uncertainty_calculator(
         profile_raw,
-        idx_day,
-        idx_saa,
+        index_day,
         profile_flatfield,
-        idx_night = None
+        background_mean=None,
+        background_std=None,
+        index_saa=None,
+        index_night=None
 ):
     """
     Computes the 1-sigma uncertainties of the profiles given 1 day of data for
@@ -133,67 +135,84 @@ def uncertainty_calculator(
 
     Args:
         profile_raw (ndarray): array of raw profiles
-        idx_day (ndarray): array of day indices
-        idx_saa (ndarray): array of saa indices
+        index_day (ndarray): array of day indices
         profile_flatfield (ndarray): array of flatfielding coefficients for
             the science pixels
-        idx_night (ndarray): array of night indices, optional
+        background_mean (ndarray): mean values of the flatfielded backgrounds
+            If a single altitude profile is given, it will be assumed constant
+            along all the day. optional
+        background_std (ndarray): standard deviation values of the flatfielded
+            backgrounds. If a single altitude profile is given, it will be
+            assumed constant along all the day. optional
+        index_saa (ndarray): array of saa indices. optional
+        index_night (ndarray): array of night indices. optional
 
     Returns:
         uncertainty_profile (ndarray): array of uncertainties, same shape as
             profile_raw
         gain_day (float): computed gain value from day profiles
-        gain_night (float): computed gain value from night profiles, optional
+        gain_night (float): computed gain value from night profiles. optional
     """
     # perform flatfielding
     profile_ff = profile_raw / profile_flatfield
 
-    # take the saa indices to calculate the background parameters
-    background_saa = profile_ff[idx_saa]
+    if background_mean is not None:
+        # if a single altitude profile is provided, copy it along time indices
+        if len(np.squeeze(background_mean).shape) == 1:
+            background_mean = np.repeat(
+                background_mean[np.newaxis,:], len(profile_ff), axis=0
+            )
+            background_std = np.repeat(
+                background_std[np.newaxis,:], len(profile_ff), axis=0
+            )
 
-    # calculate the mean and standard deviation of the background using saa data
-    background_mean_altitude = np.median(background_saa, axis=0)
-    background_std_altitude = np.std(background_saa, axis=0)
+    else:
+        # take the saa indices to calculate the background parameters
+        background_saa = profile_ff[index_saa]
 
-    # replicate the mean and the deviation along the time axis (since we assume
-    # they don't change over time)
-    background_mean = np.repeat(
-        background_mean_altitude[np.newaxis,:], len(profile_ff), axis=0
-    )
-    background_std = np.repeat(
-        background_std_altitude[np.newaxis,:], len(profile_ff), axis=0
-    )
+        # calculate the mean and standard deviation of the background using saa data
+        background_mean_altitude = np.median(background_saa, axis=0)
+        background_std_altitude = np.std(background_saa, axis=0)
+
+        # replicate the mean and the deviation along the time axis (since we assume
+        # they don't change over time)
+        background_mean = np.repeat(
+            background_mean_altitude[np.newaxis,:], len(profile_ff), axis=0
+        )
+        background_std = np.repeat(
+            background_std_altitude[np.newaxis,:], len(profile_ff), axis=0
+        )
 
     # subtract the mean background from the total signal to estimate the actual signal
     signal = profile_ff - background_mean
 
     # compute the gain for the day
     gain_day = gain_calculator_day(
-        signal[idx_day],
-        background_std[idx_day],
-        profile_flatfield[idx_day]
+        signal[index_day],
+        background_std[index_day],
+        profile_flatfield[index_day]
     )
 
     signal_variance = background_std**2
-    signal_variance[idx_day] = (
-        (gain_day / profile_flatfield[idx_day]) *
-        abs(signal[idx_day])
+    signal_variance[index_day] = (
+        (gain_day / profile_flatfield[index_day]) *
+        abs(signal[index_day])
     )
 
-    if idx_night is not None:
+    if index_night is not None:
         # compute the gain for the night
         gain_night = gain_calculator_night(
-            signal[idx_night],
-            profile_flatfield[idx_night]
+            signal[index_night],
+            profile_flatfield[index_night]
         )
 
         # make sure that the calculated night gain is larger than day (because
         # of higher voltage) . otherwise, equate it to the day gain
         gain_night = max(gain_day, gain_night)
 
-        signal_variance[idx_night] = (
-            (gain_night / profile_flatfield[idx_night]) *
-            abs(signal[idx_night])
+        signal_variance[index_night] = (
+            (gain_night / profile_flatfield[index_night]) *
+            abs(signal[index_night])
         )
 
         uncertainty_profile = np.sqrt(signal_variance + background_std**2)
