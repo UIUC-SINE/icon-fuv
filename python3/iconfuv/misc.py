22b11c46134fc8f087d4f722696b015b377341c2
ukamaci <kamaci.ulas@gmail.com>
2021-09-13 17:39:10 -0500

    Update python3/misc.py to not depend on airglow.

--- python3/iconfuv/misc.py
+++ python3/iconfuv/misc.py
@@ -1,6 +1,4 @@
-import os, glob, netCDF4, pyglow
-from airglow.FUV_L2 import get_msisGPI, FUV_Level_2_Density_Calculation, find_hm_Nm_F2, nan_checker
-import airglow.ICON_FUV_fwd_model as FUV_F # for simulating data
+import os, glob, netCDF4
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.draw import line_aa
@@ -36,88 +34,7 @@ def profiler(l1, err=False, clean=False):
     return profiles
 
 def get_oplus(l1=None, anc=None, epoch=100, stripe=3, bkgcor=False):
-    limb = 150.
-    contribution ='RRMN'
-    reg_method = 'Tikhonov'
-    weight_resid = False
-    Spherical = True
-    regu_order = 2
-    reg_param = 2500.
-
-    file_GPI = path_dir + 'ICON_Ancillary_GPI_2015-001-to-2020-187_v01r000.NC'
-    gpi = netCDF4.Dataset(file_GPI, mode='r')
-
-    mirror_dir = ['M9','M6','M3','P0','P3','P6']
-
-    mode = l1.variables['ICON_L1_FUV_Mode'][:]
-    idx = np.where(mode==2)[0][epoch]
-    dn = parser.parse(anc.variables['ICON_ANCILLARY_FUV_TIME_UTC'][idx])
-
-    # Read the geophysical indeces
-    ap3 = gpi['ap3'][:]
-    ap = gpi['ap'][:]
-    year_day = gpi['year_day'][:]
-    f107 = gpi['f107d'][:]
-    # Make sure this GPI has the average f107 in it
-    if 'f107a' in gpi.variables.keys():
-        f107a = gpi['f107a'][:]
-    else:
-        f107a = gpi['f107d'][:]
-    gpi.close()
-
-    my_f107, my_f107a, my_f107p, my_apmsis = get_msisGPI(dn, year_day, f107, f107a, ap, ap3)
-
-    tanalts = anc.variables['ICON_ANCILLARY_FUVA_TANGENTPOINTS_LATLONALT'][idx,:,stripe,2]
-    tanlons = anc.variables['ICON_ANCILLARY_FUVA_TANGENTPOINTS_LATLONALT'][idx,:,stripe,1]
-    tanlats = anc.variables['ICON_ANCILLARY_FUVA_TANGENTPOINTS_LATLONALT'][idx,:,stripe,0]
-    satlatlonalt = [
-        anc.variables['ICON_ANCILLARY_FUV_LATITUDE'][idx],
-        anc.variables['ICON_ANCILLARY_FUV_LONGITUDE'][idx],
-        anc.variables['ICON_ANCILLARY_FUV_ALTITUDE'][idx]
-    ]
-    local_time = anc.variables['ICON_ANCILLARY_FUVA_TANGENTPOINTS_LST'][idx, -1, 2]
-
-    limb_i = np.where(np.squeeze(tanalts)>=limb)[0]
-    br = l1.variables['ICON_L1_FUVA_SWP_PROF_%s_CLEAN' % mirror_dir[stripe]][idx,:]
-    if bkgcor is True:
-        # br -= np.nanmedian(br[tanalts>400])
-        med = np.median(br[10:50])
-        med=0 if med>0 else med
-        br -= med
-    br = br[limb_i]
-    unmasked_ind = nan_checker(br)
-    limb_i0 = limb_i[unmasked_ind]
-    br = br[::-1]
-    unmasked_ind_f = nan_checker(br)
-    limb_i = limb_i[unmasked_ind_f]
-    br = br[unmasked_ind_f]
-    err = l1.variables['ICON_L1_FUVA_SWP_PROF_%s_Error' % mirror_dir[stripe]][idx,limb_i0]
-    err = err[::-1]
-    err[np.where(err<1e-1)] = 1 # set the error to 1 if it is 0
-    h = np.squeeze(tanalts[limb_i0])
-    h = h[::-1]
-    az = np.squeeze(anc.variables['ICON_ANCILLARY_FUVA_FOV_AZIMUTH_ANGLE'][idx,limb_i0,stripe])
-    az = az[::-1]
-    ze = np.squeeze(anc.variables['ICON_ANCILLARY_FUVA_FOV_ZENITH_ANGLE'][idx,limb_i0,stripe])
-    ze = ze[::-1]
-    tanlons = tanlons[limb_i0]
-    tanlats = tanlats[limb_i0]
-    tanlons = tanlons[::-1]
-    tanlats = tanlats[::-1]
-    br[br<0] = 0
-
-    ver,Ne,h_centered,Sig_ver,Sig_Ne = FUV_Level_2_Density_Calculation(
-        br,h,satlatlonalt,az,ze,
-        Sig_Bright = np.diag(err**2), weight_resid=False,
-        limb = limb,Spherical = Spherical, reg_method = reg_method,
-        regu_order = regu_order, contribution =contribution,dn = dn,
-        f107=my_f107, f107a=my_f107a, f107p=my_f107p, apmsis=my_apmsis,
-        reg_param=reg_param
-    )
-
-    return Ne, h_centered
-
-
+    raise RuntimeError('This function has been moved to airglow.misc')
 
 def index_finder(data, utc, type='l2'):
     str = 'ICON_L25_UTC_Time' if type=='l2' else 'ICON_L1_FUVA_SWP_Center_Times'
@@ -331,10 +248,16 @@ def get_br(date='2020-01-03', epoch=300, stripe=None, v=3, r=0, size='full', swa
     return br, br_err
 
 
-def get_br_nights(l1):
+def get_br_nights(l1, anc=None):
     mirror_dir = ['M9','M6','M3','P0','P3','P6']
     mode = l1.variables['ICON_L1_FUV_Mode'][:]
     mode_night = (mode == 2).astype(np.int)
+    if anc is not None:
+        alt = anc.variables['ICON_ANCILLARY_FUVA_TANGENTPOINTS_LATLONALT'][:,:,:,2]
+        ind = np.sum(alt<300, axis=1) - 1
+        xx, yy = np.meshgrid(np.arange(alt.shape[0]), np.arange(6), indexing='ij')
+        sza = anc.variables['ICON_ANCILLARY_FUVA_TANGENTPOINTS_SZA'][:]
+        sza300 = np.min(sza[xx,ind,yy], axis=1)
     nights = np.diff(mode_night, prepend=0)
     nights[nights==-1] = 0
     idxs = np.where(mode==2)[0][:]
@@ -343,6 +266,7 @@ def get_br_nights(l1):
     brsc = []
     brs_err = []
     mask_arr = []
+    szas = []
     for night in np.unique(nights):
         night_ind = np.where(nights==night)[0]
         br = np.zeros((6, len(night_ind), 256))
@@ -356,11 +280,16 @@ def get_br_nights(l1):
             mask[i] = tmp.mask
             br[i] = tmp.filled(fill_value=0)
             brc[i] = tmp2.filled(fill_value=0)
+        if anc is not None:
+            szas.append(sza300[idxs[night_ind]])
         brs.append(br)
         brsc.append(brc)
         brs_err.append(br_err)
         mask_arr.append(mask)
-    return brs, brsc, brs_err, mask_arr, nights, idxs
+    if anc is not None:
+        return brs, brsc, szas, brs_err, mask_arr, nights, idxs
+    else:
+        return brs, brsc, brs_err, mask_arr, nights, idxs
 
 
 def shiftappend(im, w=[5,5]):
